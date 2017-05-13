313f4f6eb1264a24336d0185b975e1f549479efc
“Paulo <paulo.sorrentino@luizalabs.com>
2017-05-13 12:50:47 -0300

    Inicio

---
+++ loader.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+const server = require('./src/config/server')
+require('./src/config/database')
+require('./src/config/routes')(server)
---
+++ src/config/database.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+module.exports = mongoose.connect(process.env.MONGODB_URI)
